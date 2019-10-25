0b3c431b6a7914fa069e99fd8131032f52c46121
khuongsatou <yoshi240499@gmail.com>
2019-10-26 02:52:44 +0700

    Razor systax

---
+++ ASPMVCRazor/ASPMVCRazor/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPMVCRazor.Models;
+
+namespace ASPMVCRazor.Controllers
+{
+    public class StudentController : Controller
+    {
+
+          IList<Student> studentList = new List<Student>{ 
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 } 
+         };
+        Student std = new Student();
+
+        //
+        // GET: /Student/
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+      
+	}
+}
---
+++ ASPMVCRazor/ASPMVCRazor/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ASPMVCRazor.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        //thêm thuộc tính tên hiển thị
+        //Ngoài file index home kia chưa khai báo thuộc tính
+        //À phải có thư viện DataAnnotations mới có thuộc tính
+        [Display(Name="Name")]
+        public String StudentName { get; set; }
+        public int Age { get; set; }
+
+        public string Description { get; set; }
+
+        public bool isNewlyEnrolled { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DoB { get; set; }
+
+
+
+        public Gender StudentGender { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    }
+}
